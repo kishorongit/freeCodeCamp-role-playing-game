b29de1bfd0d4e58c67bbf74e336fe8741d18024e
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-08-04 15:10:24 +0545

    feat(fight): add dodge, defeat monster, and lose functions

--- script.js
+++ script.js
@@ -79,6 +79,12 @@ const locations = [
         "button text": ["Attack", "Dodge", "Run"],
         "button functions": [attack, dodge, goTown],
         text: "You are fighting a monster."
+    },
+    {
+        name: "kill monster",
+        "button text": ["Go to town square", "Go to town square", "Go to town square"],
+        "button functions": [goTown, goTown, goTown],
+        text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
     }
 ];
 
@@ -200,8 +206,27 @@ function attack() {
     text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
     health -= monsters[fighting].level;
     monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+    if (health <= 0) {
+        lose();
+    } else if (monsterHealth <= 0) {
+        defeatMonster();
+    }
 }
 
 function dodge() {
+    text.innerText = "You dodge the attack from the " + monsters[fighting].name + ".";
+}
+
+function defeatMonster() {
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    update(locations[4]);
+}
+
+function lose() {
   
 }
