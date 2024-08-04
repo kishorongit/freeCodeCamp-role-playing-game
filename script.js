58f6cfd0bbb69d4aa9cfdba64abd67b84da26889
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-08-04 13:40:08 +0545

    feat(store): add sellWeapon method implementation

--- script.js
+++ script.js
@@ -131,6 +131,20 @@ function buyWeapon() {
     }
 }
 
+// Sell weapon
+function sellWeapon() {
+    // Players should not be able to sell their only weapon
+    if (inventory.length > 1) {
+        gold += 15;
+        goldText.innerText = gold;
+        let currentWeapon = inventory.shift();
+        text.innerText = "You sold a " + currentWeapon + ".";
+        text.innerText += " In your inventory you have: " + inventory;
+    } else {
+        text.innerText = "Don't sell your only weapon!";
+    }
+}
+
 // Fight slime
 function fightSlime() {
 
