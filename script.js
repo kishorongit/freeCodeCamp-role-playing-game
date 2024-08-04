6f62978e0baedaba1aa668fb0edd038d1e284000
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-08-04 11:16:47 +0545

    feat(dashboard): initialize methods for interactivity

--- script.js
+++ script.js
@@ -20,3 +20,47 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+
+// Initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+// Go to store
+function goStore() {
+    // Update button text
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    
+    // Update onclick method
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+
+    text.innerText = "You enter the store."
+}
+
+// Go to cave
+function goCave() {
+    console.log("Going to cave.")
+}
+
+// Fight Dragon
+function fightDragon() {
+    console.log("Fighting dragon.");
+}
+
+// Buy health
+function buyHealth() {
+    console.log("Buying health.")
+}
+// Buy weapon
+function buyWeapon() {
+    console.log("Buying weapon.")
+}
+
+// Go to town square
+function goTown() {
+    console.log("Going to town square.")
+}
